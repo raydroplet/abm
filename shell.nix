4b45faf46944c7966365b863138c53d38de48f48
Ray Droplet <262660609+raydroplet@users.noreply.github.com>
2025-12-24 02:14:43 +0000

    add gui framework

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    wayland
+    libxkbcommon
+  ];
+
+shellHook = ''
+  export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+    pkgs.wayland
+    pkgs.libxkbcommon
+    pkgs.libglvnd
+  ]}:$LD_LIBRARY_PATH
+'';
+}
